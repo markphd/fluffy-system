c0d0737c23d79f03d8d2d34271e9e94fc245a1c7
Mark Philip D <radical.mark@gmail.com>
2016-09-20 04:11:29 +0400

    added style in frontpage

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Supporter = require('../models/supporter.model.js');
 var scheduleCtrl = require('../controllers/morning.schedule.controller.js');
+var moment = require('moment');
+var week = moment().isoWeek();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { week: week });
 });
 
 // GET MORNING VIEW
